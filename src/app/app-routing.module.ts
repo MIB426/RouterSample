0cb43c7f14c2b2f02413f9cfe7b3e19ce22592c6
James Wu <MIB426@htmail.com>
2019-05-30 16:04:24 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { ComponentOneComponent } from './component-one.component';
+import { ComponentTwoComponent } from './component-two.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'component-one', pathMatch: 'full' },
+  { path: 'component-one', component: ComponentOneComponent },
+  { path: 'component-two', component: ComponentTwoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
